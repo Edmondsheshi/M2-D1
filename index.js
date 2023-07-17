b769ec75909f06eb5a6470c48d0d884f6442ee0b
Edmondsheshi <120511977+Edmondsheshi@users.noreply.github.com>
2023-07-17 21:02:30 +0200

    M2 D1

---
+++ index.js
@@ -0,0 +1,26 @@
+// 1. Cos'è un algoritmo? Prova a spiegarlo con parole tue, come se lo dovessi spiegare a qualcuno che non ne capisce niente di informatica.
+
+//per algoritmo si intende una successione di istruzioni o passi
+//che definiscono le operazioni da eseguire sui dati per ottenere i risultati.
+//Lo schema esecutivo di un algoritmo specifica che i passi devono essere eseguiti 
+//in sequenza, Volendo fare un esempio di algoritmo in informatica, i motori di ricerca come Google sono
+//basati proprio su questo concetto per poter rispondere quanto più coerentemente alla richiesta di un utente.
+
+//2. Cos'è una variabile? Prova a spiegarlo con parole tue, come se lo dovessi spiegare a qualcuno che non ne capisce niente di informatica.
+
+//Nei più semplici linguaggi di scripting interpretati, le variabili sono, di norma, solo semplici identificatori privi di un tipo e una dimensione definita.
+// Lo spazio di memoria è allocato dinamicamente al momento dell'assegnamento.
+
+//3. undefined e null sono la stessa cosa (falso)
+
+//Undefined può riferirsi a: Qualcosa che non ha la definizione, Non definita, riferendosi ad un'operazione che non può essere definita significativamente 
+
+//Null, in un contesto di database, è l'assenza totale di un valore in un determinato campo e significa che il valore del campo è sconosciuto.
+//Null non equivale a un valore zero per un campo numerico, un campo di testo o un valore di spazio.
+
+
+// alla domanda n.3 la risposta è falsa perchè sono 2 cose completamente diverse.... come scritto sopra, undefined significa che il conteto
+//non è stato definito in maniera adeguata e l'operazione non puo' essere eseguita correttamente
+
+// invece null, l'oggetto non è proprio inteso.. non si puo' utilizzare in qualciasi maniera perchè mancante di valori
+
